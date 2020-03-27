844a62391410dcc1118de7a245539dc014825830
zaoying <505155730@qq.com>
2020-03-27 14:53:12 +0800

    improve settimeout

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ const fileReader = require('./file_reader')
 const spider = require('./spider')
 
 let timeout = 0;
-
 fileReader.readFile('./urls.txt')
-.call(this, repo => {
-    if (repo.host === 'github.com') {
-        setTimeout(() => {
-            spider.getRepos(repo.author, repo.name)
-            .call(this, console.log)
-        }, timeout)
-        
-
-        setTimeout(() => {
-            spider.getRelease(repo.author, repo.name)
-                .call(console.log)
-        }, timeout)
-
-        timeout += 100;
-    }
-})
+    .call(this, repo => {
+        if (repo.host === 'github.com') {
+            console.log("倒计时:" + timeout)
+            setTimeout((author, name) => {
+                console.info(author + ':' + name)
+                // spider.getRepos(author, name)
+                //     .call(this, console.log)
+                
+                // spider.getRelease(author, name)
+                //     .call(console.log)
+            }, timeout, repo.author, repo.name)
+            timeout += 10;
+        }
+    })
