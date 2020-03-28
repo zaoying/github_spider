aedfe09399b7457d2c8d97cae68ee9e9001c7caf
zaoying <505155730@qq.com>
2020-03-28 10:18:52 +0800

    print key

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ const fileReader = require('./file_reader')
 const spider = require('./spider')
 const path = require('path');
 
+async function handler(author, name) {
+    let keys = []
+
+    let info = await spider.getRepos(author, name)
+    keys.push(info.keys)
+
+    let release = await spider.getRelease(author, name)
+    keys.push(release.keys)
+
+    console.log(keys.join('\t'))
+}
+
 let urls = path.resolve(__dirname, "../assets/urls.txt");
 let timeout = 0;
 
-fileReader.lines(urls)
-    .call(this, repo => {
-        if (repo.host === 'github.com' && repo.name) {
-            setTimeout((author, name) => {
-                spider.getRepos(author, name).then(console.log)
-                spider.getRelease(author, name).then(console.log)
-            }, timeout, repo.author, repo.name)
-        }
-        timeout += 1000;
-    })
+fileReader.lines(urls).call(this, repo => {
+    repo.host === 'github.com' && repo.name && setTimeout(handler, timeout, repo.author, repo.name)
+    timeout += 1000
+})
