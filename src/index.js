66450809da1ee683aa4381d563ca49e1328aacfa
zaoying <505155730@qq.com>
2020-04-01 11:16:09 +0800

    解决重构repo.transform函数引发的问题

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ const path = require('path')
 let urls = path.resolve(__dirname, "../assets/urls.txt");
 let output = path.resolve(__dirname, "../output/");
 let saveFile = repo.output(output, "/result.txt");
+let tranform = repo.transform()
 
 fileUitls.lines(urls)
     .subscribe(async line => {
         let repository = repo.compose(line)
         let info = await repo.resolve(repository)
-        let values = repo.transform(info)
+        let values = tranform(info)
         saveFile.call(this, values)
     })
