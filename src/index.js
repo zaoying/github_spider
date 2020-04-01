41da3f3e7ceb7ef7426757e2bc12be97464b9829
zaoying <505155730@qq.com>
2020-04-01 11:21:37 +0800

    修复拼写错误

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const path = require('path')
 let urls = path.resolve(__dirname, "../assets/urls.txt");
 let output = path.resolve(__dirname, "../output/");
 let saveFile = repo.output(output, "/result.txt");
-let tranform = repo.transform()
+let transform = repo.transform()
 
 fileUitls.lines(urls)
     .subscribe(async line => {
         let repository = repo.compose(line)
         let info = await repo.resolve(repository)
-        let values = tranform(info)
-        saveFile.call(this, values)
+        let values = transform(info)
+        saveFile(values)
     })
